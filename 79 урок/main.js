17b18d4c75e1c4a8f0eecd9a32a5c38585326bd5
drshen05 <139790560+drshen05@users.noreply.github.com>
2023-08-22 21:39:22 +0600

    Add files via upload

---
+++ 79 урок/main.js
@@ -0,0 +1,11 @@
+// 1) const age = 18
+// if(age >= 18) {
+//     console.log('Вы совершеннолетний')
+// } else {
+//     console.log('Вам еще нет 18')
+// }
+
+// 2)
+const age = 16
+
+age >= 18 ? console.log('Вы совершеннолетний') : console.log('Вам еще нет 18')
