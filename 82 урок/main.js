4e62d3cb055bcd07dd93ab49cef106fc7ba892ba
drshen05 <139790560+drshen05@users.noreply.github.com>
2023-08-31 22:03:22 +0600

    Add files via upload

---
+++ 82 урок/main.js
@@ -0,0 +1,29 @@
+// 1)
+// function myName(name){
+//     return 'Привет, ' + name
+// }
+
+// console.log(myName('Дарья'))
+
+
+// 2)
+const numbers = [1, 2, 3, 4, 5,6, 7, 8, 9, 10, 11, 12, 13]
+
+function array(numbers){
+    for ( let i = 0; i < numbers.length; i++) {
+
+    if ( numbers[i] > 10 ){
+        console.log(numbers[i])
+    }
+
+}
+}
+array(numbers)
+
+
+// 3)
+
+// function сalculator(firstNumber, secondNumber, sign) {
+//     return 2 - 3
+    
+// }console.log(сalculator(2, 3,'minus' ))
