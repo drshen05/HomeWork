88568e412530866d8e73f689e017454de5543596
drshen05 <139790560+drshen05@users.noreply.github.com>
2023-09-02 15:32:32 +0600

    Add files via upload

--- 82 урок/main.js
+++ 82 урок/main.js
@@ -7,23 +7,58 @@
 
 
 // 2)
-const numbers = [1, 2, 3, 4, 5,6, 7, 8, 9, 10, 11, 12, 13]
+// const numbers = [1, 2, 3, 4, 5,6, 7, 8, 9, 10, 11, 12, 13]
 
-function array(numbers){
-    for ( let i = 0; i < numbers.length; i++) {
+// function array(numbers){
+//     for ( let i = 0; i < numbers.length; i++) {
 
-    if ( numbers[i] > 10 ){
-        console.log(numbers[i])
-    }
+//     if ( numbers[i] > 10 ){
+//         console.log(numbers[i])
+//     }
 
-}
-}
-array(numbers)
+// }
+// }
+// array(numbers)
 
 
 // 3)
 
 // function сalculator(firstNumber, secondNumber, sign) {
-//     return 2 - 3
-    
-// }console.log(сalculator(2, 3,'minus' ))
+//     result = firstNumber - secondNumber
+//     if (sign !== '-') {
+//         console.log(result)
+
+//     }
+
+
+// } console.log(сalculator(2, 3, 'minus'))
+
+// function сalculator(firstNumber, secondNumber, sign) {
+//     result = firstNumber + secondNumber
+//     if (sign !== '+') {
+//         console.log(result)
+
+//     }
+
+
+// } console.log(сalculator(2, 3, 'plus'))
+
+// function сalculator(firstNumber, secondNumber, sign) {
+//     result = firstNumber * secondNumber
+//     if (sign !== '*') {
+//         console.log(result)
+
+//     }
+
+
+// } console.log(сalculator(2, 3, 'multiplication'))
+
+function сalculator(firstNumber, secondNumber, sign) {
+    result = firstNumber / secondNumber
+    if (sign !== '/') {
+        console.log(result)
+
+    }
+
+
+} console.log(сalculator(2, 3, 'division'))
