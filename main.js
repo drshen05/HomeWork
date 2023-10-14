247c3ac151aefdcacba40e56ed25db6ca4a33d63
drshen05 <139790560+drshen05@users.noreply.github.com>
2023-10-14 17:46:20 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,54 +1,34 @@
-// 1)
-// function myName(name){
-//     return 'Привет, ' + name
-// }
-
-// console.log(myName('Дарья'))
-
-
-// 2)
-// const numbers = [1, 2, 3, 4, 5,6, 7, 8, 9, 10, 11, 12, 13]
-
-// function array(numbers){
-//     for ( let i = 0; i < numbers.length; i++) {
-
-//     if ( numbers[i] > 10 ){
-//         console.log(numbers[i])
+// const user = {
+//     daria: {
+//         age: 18,
+//         isAdmin: true,
+//         sayHello(name) {
+//             console.log(`Hello ${name}`)
+//         }
 //     }
-
 // }
-// }
-// array(numbers)
+// console.log(user.daria)
+// user.daria.sayHello(`daria`)
 
 
-// 3)
-function calculate(num1, num2, sign) {
-    let result
-
-    for (let i = 0; i < 1; i++) {
-        if (sign === 'plus') {
-            result = num1 + num2
-        } else if (sign === 'minus') {
-            result = num1 - num2
-        } else if (sign === 'multiply') {
-            result = num1 * num2
-        } else if (sign === 'divide') {
-            result = num1 / num2;
-        }
+const users = [
+    {
+        name: 'alex',
+        age: 23,
+        isAdmin: true
+    },
+    {
+        name: 'masha',
+        age: 19,
+        isAdmin:false
+    },
+    {
+        name: 'sasha',
+        age: 20,
+        isAdmin:true
     }
-
-    return result;
-}
-
-
-const additionResult = calculate(2, 3, 'plus')
-console.log(additionResult)
-
-const subtractionResult = calculate(5, 2, 'minus')
-console.log(subtractionResult)
-
-const multiplicationResult = calculate(4, 6, 'multiply')
-console.log(multiplicationResult)
-
-const divisionResult = calculate(10, 2, 'divide')
-console.log(divisionResult)
+]
+var quantity = 3
+for(let i = 0; i < users.length; i++)
+console.log(users[i].isAdmin + 1)
+console.log(quantity)
